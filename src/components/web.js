31ded8c9175acb1a3214d9e4da87601109959de0
Sean Yesmunt <sean.yesmunt@gmail.com>
2017-02-08 23:50:58 -0800

    add activity indicator for webview

--- src/components/web.js
+++ src/components/web.js
@@ -1,18 +1,32 @@
 import React from 'react'
 import {
+  Text,
   WebView,
-  StyleSheet
+  StyleSheet,
+  ActivityIndicator
 } from 'react-native'
 
 const Web = (props) => {
   const { url } = props
   return (
-    <WebView style={styles.web} source={{uri: url}} />
+    <WebView
+      source={{uri: url}}
+      style={styles.web}
+      startInLoadingState={true}
+      renderLoading={() => (
+        <ActivityIndicator
+          style={styles.spinner}
+          color='#125580'
+          size='large' />
+      )} />
   )
 }
 const styles = StyleSheet.create({
   web: {
     flex: 1
+  },
+  spinner: {
+    paddingTop: 40
   }
 })
 
